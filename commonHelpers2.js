ebf3327225227dffccbcbb90b8a3fab765d965e2
DimaShcherbiuk <147422642+DimaShcherbiuk@users.noreply.github.com>
2024-03-10 15:37:04 +0000

    Deploying to gh-pages from @ DimaShcherbiuk/goit-js-hw-09@d563820b677698e1cd2e332d5fdfa3fdc9c59b7d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/styles-6d8181f3.js";const o=document.querySelector("form.feedback-form");let l=document.querySelector('[name="email"]'),m=document.querySelector('[name="message"]');o.addEventListener("input",s);o.addEventListener("submit",n);let t=localStorage.getItem("feedback-form-state");if(t){let e=JSON.parse(t);l.value=e.email,m.value=e.message}function s(e){e.preventDefault();const a={email:l.value,message:m.value};let r=JSON.stringify(a);localStorage.setItem("feedback-form-state",r)}function n(e){e.preventDefault();let a=JSON.parse(t);console.log(`Об'єкт feedback-form-state:
- email: ${a.email} 
- message: ${a.message}`),l.value="",m.value="",localStorage.removeItem("feedback-form-state")}
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const o=document.querySelector("form.feedback-form");let l=document.querySelector('[name="email"]'),m=document.querySelector('[name="message"]');o.addEventListener("input",s);o.addEventListener("submit",n);let a=localStorage.getItem("feedback-form-state");if(a){let e=JSON.parse(a);l.value=e.email,m.value=e.message}function s(e){e.preventDefault();const t={email:l.value,message:m.value};let r=JSON.stringify(t);localStorage.setItem("feedback-form-state",r)}function n(e){e.preventDefault();let t=JSON.parse(a);console.log(`Об'єкт feedback-form-state:
+ email: ${t.email} 
+ message: ${t.message}`),l.value="",m.value="",localStorage.removeItem("feedback-form-state")}
 //# sourceMappingURL=commonHelpers2.js.map
